76bbbcfe29ab26f8216e3390ba78b3f34e795866
guiaraujofreitas <guiaraujofreitas@outlook.com>
2023-05-11 19:55:21 -0300

    first commit

---
+++ src/main.py
@@ -0,0 +1,13 @@
+
+def print_messege():
+    print (f 'Os valores {n1} e {n2} somados dão' {n1 +n2} )
+
+    return None
+
+def main():
+
+    n1,n2 = gether_data()
+
+    print_messege(n1, n2)
+
+    None
